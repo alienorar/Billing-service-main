5ece9086c6353ce482221e65ebb8fe6f8c7f9b4c
alienorar <rayhonakhayrullaeva@gmail.com>
2025-06-11 11:40:26 +0500

    speciality unused func removed

--- src/modules/speciality/pages/modal.tsx
+++ src/modules/speciality/pages/modal.tsx
@@ -1,51 +1,18 @@
-import { Drawer, Form, Input, Button} from "antd";
-import { useEffect } from "react";
-import { useCreateSpeciality, useUpdateSpeciality } from "../hooks/mutations";
+import { Drawer, Form, Input, Button } from "antd";
+import { useCreateSpeciality } from "../hooks/mutations";
 import { SpecialityType } from "@types";
 
-const SpecialityDrawer = ({ open, handleClose, update }: any) => {
+const SpecialityDrawer = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
     const [form] = Form.useForm();
     const { mutate: createMutate, isPending: isCreating } = useCreateSpeciality();
-    const { mutate: updateMutate, isPending: isUpdating } = useUpdateSpeciality();
-
-    useEffect(() => {
-        console.log(
-            update,"jkdbjkfbdbvj"
-        );
-        
-        if (update?.specialityCode) {
-            form.setFieldsValue({
-                id: update.id,
-                specialityCode: update.specialityCode,
-                specialityName: update.specialityName,
-                contractCost: update.contractCost,
-                contractCostInLetters: update.contractCostInLetters,
-                duration: update.duration,
-                educationForm: update.educationForm,
-                educationType: update.educationType,
-                educationLang: update.educationLang,
-            });
-        } else {
-            form.resetFields();
-        }
-    }, [update, form]);
 
     const onFinish = async (values: SpecialityType) => {
-        const payload = {
-            ...values,
-            id: update.id,
-        };
-
-        if (update?.specialityCode) {
-            updateMutate(payload, { onSuccess: handleClose });
-        } else {
-            createMutate(payload, { onSuccess: handleClose });
-        }
+        createMutate(values, { onSuccess: handleClose });
     };
 
     return (
         <Drawer
-            title={update?.specialityCode ? "Edit Speciality" : "Add New Speciality"}
+            title="Add New Speciality"
             placement="right"
             open={open}
             onClose={handleClose}
@@ -78,7 +45,6 @@ const SpecialityDrawer = ({ open, handleClose, update }: any) => {
 
                 <Form.Item label="Education Type" name="educationType">
                     <Input style={{ padding: "6px", border: "1px solid #d9d9d9", borderRadius: "6px" }} />
-
                 </Form.Item>
 
                 <Form.Item label="Education Language" name="educationLang">
@@ -86,8 +52,8 @@ const SpecialityDrawer = ({ open, handleClose, update }: any) => {
                 </Form.Item>
 
                 <Form.Item>
-                    <Button block htmlType="submit" style={{ backgroundColor: "#050556", color: "white" }} loading={isCreating || isUpdating}>
-                        {update?.specialityCode ? "Update Speciality" : "Create Speciality"}
+                    <Button block htmlType="submit" style={{ backgroundColor: "#050556", color: "white" }} loading={isCreating}>
+                        Create Speciality
                     </Button>
                 </Form.Item>
             </Form>
