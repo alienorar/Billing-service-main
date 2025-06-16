2342e3189a5a03bcf6724c56fdff480a1ca63fb6
alienorar <rayhonakhayrullaeva@gmail.com>
2025-06-16 17:52:21 +0500

    trinfo fixed

--- src/modules/one-student/pages/index.tsx
+++ src/modules/one-student/pages/index.tsx
@@ -200,10 +200,10 @@ const StudentDetails: React.FC = () => {
                 <svg className="w-5 h-5 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                Shartnoma summasi
+                Shartnoma summasi : 
               </span>
               <span className="text-sm font-bold text-gray-800">
-                {trInfo?.totalMustPaidAmount ? Number(trInfo.totalMustPaidAmount).toLocaleString() : "0"} UZS
+                {trInfo?.totalContractAmount ? Number(trInfo.totalContractAmount).toLocaleString() : "0"} UZS
               </span>
             </div>
             <div className="flex justify-between items-center">
@@ -236,9 +236,9 @@ const StudentDetails: React.FC = () => {
                 Qarzdorlik
               </span>
               <span className="text-sm font-bold text-red-600">
-                {trInfo
-                  ? (Number(trInfo.totalMustPaidAmount) - Number(trInfo.totalPaidAmount) - Number(trInfo.totalDiscountAmount)).toLocaleString()
-                  : "0"} UZS
+              
+                   {Number(trInfo.totalMustPaidAmount).toLocaleString()}
+                  UZS
               </span>
             </div>
           </div>
